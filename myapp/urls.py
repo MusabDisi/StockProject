b825a723d222fe737ade647d17f1b7c4e0f20fef
MusabDisi <m.dissi@outlook.com>
2020-10-12 23:31:15 +0300

    added urls again
    urls was removed while resolving merge conflict

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', views.logout_view, name='logout'),
     path('accounts/register/', views.register, name='register'),
+    path('accounts/register/', views.register, name='register'),
+    path('accounts/profile/', views.user_profile, name='profile'),
+    path('accounts/edit_profile/', views.edit_profile, name='edit_profile'),
 ]
 
 if settings.DEBUG:
